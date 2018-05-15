4300b402e9bfe1418936670c762c700c8d8680c3
bdegley4789 <egleyb@oregonstate.edu>
2018-05-14 23:09:37 -0700

    Fix compile errors add predator

--- project4_cs475/project_4.cpp
+++ project4_cs475/project_4.cpp
@@ -6,17 +6,20 @@
 #include <math.h>
 #include <omp.h>
 
-int    NowYear;        // 2017 - 2022
-int    NowMonth;        // 0 - 11
+// starting date and time:
+int    NowYear = 2017;        // 2017 - 2022
+int    NowMonth = 0;        // 0 - 11
 
 float    NowPrecip;        // inches of rain per month
 float    NowTemp;        // temperature this month
-float    NowHeight;        // grain height in inches
-int    NowNumDeer;        // number of deer in the current population
+// starting state (feel free to change this if you want):
+float    NowHeight = 1.0;        // grain height in inches
+int    NowNumDeer = 2;        // number of deer in the current population
+int NowNumPredator = 1;         //number of predators in current population
 unsigned int seed = 0;  // a thread-private variable
-float x = Ranf( &seed;, -1.f, 1.f );
 
 const float GRAIN_GROWS_PER_MONTH =        8.0;
+const float ONE_PREDATOR_EATS_PER_MONTH = 1;
 const float ONE_DEER_EATS_PER_MONTH =        0.5;
 
 const float AVG_PRECIP_PER_MONTH =        6.0;    // average
@@ -29,104 +32,6 @@ const float RANDOM_TEMP =            10.0;    // plus or minus noise
 
 const float MIDTEMP =                40.0;
 const float MIDPRECIP =                10.0;
-
-// starting date and time:
-NowMonth =    0;
-NowYear  = 2017;
-
-// starting state (feel free to change this if you want):
-NowNumDeer = 1;
-NowHeight =  1.;
-
-void GrainDeer() {
-    while( NowYear < 2023 )
-    {
-        // compute a temporary next-value for this quantity
-        // based on the current state of the simulation:
-        . . .
-        
-        // DoneComputing barrier:
-        #pragma omp barrier
-        . . .
-        
-        // DoneAssigning barrier:
-        #pragma omp barrier
-        . . .
-        
-        // DonePrinting barrier:
-        #pragma omp barrier
-        . . .
-    }
-}
-
-void Grain(){
-    while( NowYear < 2023 )
-    {
-        // compute a temporary next-value for this quantity
-        // based on the current state of the simulation:
-        . . .
-        
-        // DoneComputing barrier:
-        #pragma omp barrier
-        . . .
-        
-        // DoneAssigning barrier:
-        #pragma omp barrier
-        . . .
-        
-        // DonePrinting barrier:
-        #pragma omp barrier
-        . . .
-    }
-    float tempFactor = exp(   -SQR(  ( NowTemp - MIDTEMP ) / 10.  )   );
-    
-    float precipFactor = exp(   -SQR(  ( NowPrecip - MIDPRECIP ) / 10.  )   );
-    NowHeight += tempFactor * precipFactor * GRAIN_GROWS_PER_MONTH;
-    NowHeight -= (float)NowNumDeer * ONE_DEER_EATS_PER_MONTH;
-    
-}
-
-void Watcher() {
-    while( NowYear < 2023 )
-    {
-        // compute a temporary next-value for this quantity
-        // based on the current state of the simulation:
-        . . .
-        
-        // DoneComputing barrier:
-        #pragma omp barrier
-        . . .
-        
-        // DoneAssigning barrier:
-        #pragma omp barrier
-        . . .
-        
-        // DonePrinting barrier:
-        #pragma omp barrier
-        . . .
-    }
-}
-
-void MyAgent() {
-    while( NowYear < 2023 )
-    {
-        // compute a temporary next-value for this quantity
-        // based on the current state of the simulation:
-        . . .
-        
-        // DoneComputing barrier:
-        #pragma omp barrier
-        . . .
-        
-        // DoneAssigning barrier:
-        #pragma omp barrier
-        . . .
-        
-        // DonePrinting barrier:
-        #pragma omp barrier
-        . . .
-    }
-}
 
 float SQR( float x ) {
     return x*x;
@@ -144,21 +49,124 @@ int Ranf( unsigned int *seedp, int ilow, int ihigh ) {
     
     return (int)(  Ranf(seedp, low,high) );
 }
+float x = Ranf( &seed, -1.f, 1.f );
+
+void GrainDeer() {
+    while( NowYear < 2023 )
+    {
+        // compute a temporary next-value for this quantity
+        // based on the current state of the simulation:
+        int deer = NowNumDeer;
+        if (deer != NowHeight) {
+            if (deer > NowHeight) {
+                deer--;
+            } else {
+                deer++;
+            }
+        }
+        
+        // DoneComputing barrier:
+        #pragma omp barrier
+        NowNumDeer = deer;
+        
+        // DoneAssigning barrier:
+        #pragma omp barrier
+        
+        // DonePrinting barrier:
+        #pragma omp barrier
+    }
+}
+
+void Grain(){
+    while( NowYear < 2023 )
+    {
+        // compute a temporary next-value for this quantity
+        // based on the current state of the simulation:
+        float tempHeight = NowHeight;
+        float tempFactor = exp(-SQR((NowTemp - MIDTEMP)/ 10.0));
+        float precipFactor = exp(-SQR((NowPrecip - MIDPRECIP)/ 10.0));
+        tempHeight += tempFactor * precipFactor * GRAIN_GROWS_PER_MONTH;
+        tempHeight -= (float)NowNumDeer * ONE_DEER_EATS_PER_MONTH;
+        if (tempHeight < 0) {
+            tempHeight = 0;
+        }
+        // DoneComputing barrier:
+        #pragma omp barrier
+        NowHeight = tempHeight;
+        
+        // DoneAssigning barrier:
+        #pragma omp barrier
+        
+        // DonePrinting barrier:
+        #pragma omp barrier
+    }
+    
+}
+
+void Watcher() {
+    while( NowYear < 2023 )
+    {
+        // compute a temporary next-value for this quantity
+        // based on the current state of the simulation:
+        
+        // DoneComputing barrier:
+        #pragma omp barrier
+        
+        // DoneAssigning barrier:
+        #pragma omp barrier
+        std::cout << "NowMounth: " << NowMonth << " NowYear: " << NowYear << " NowNumDeer: " << NowNumDeer << " NowPrecip: " << NowPrecip << " NowTemp: " << NowTemp << " NowHeight: " << NowHeight << " NowNumPredator: " << NowNumPredator << "\n";
+        if(NowMonth == 11) {
+            NowMonth = 0;
+            NowYear++;
+        } else {
+            NowMonth++;
+        }
+        float ang = (30.0 * (float) NowMonth + 15.0) * (M_PI / 180.0);
+        float temp = AVG_TEMP - AMP_TEMP * cos( ang );
+        unsigned int seed = 0;
+        NowTemp = temp + Ranf( &seed, -RANDOM_TEMP, RANDOM_TEMP);
+        
+        float precip = AVG_PRECIP_PER_MONTH + AMP_PRECIP_PER_MONTH * sin( ang );
+        NowPrecip = precip + Ranf( &seed,  -RANDOM_PRECIP, RANDOM_PRECIP );
+        if( NowPrecip < 0. )
+            NowPrecip = 0.;
+        // DonePrinting barrier:
+        #pragma omp barrier
+    }
+}
+
+void Predator() {
+    while( NowYear < 2023 )
+    {
+        // compute a temporary next-value for this quantity
+        // based on the current state of the simulation:
+        int hunts = Ranf(&seed,0,5);
+        int deer = NowNumDeer;
+        //Predator has 1/5 chance of hunting and killing a deer
+        if (hunts == 1) {
+            deer--;
+        }
+        
+        // DoneComputing barrier:
+        #pragma omp barrier
+        NowNumDeer = deer;
+        
+        // DoneAssigning barrier:
+        #pragma omp barrier
+
+        
+        // DonePrinting barrier:
+        #pragma omp barrier
+
+    }
+}
 
 int main( int argc, char *argv[ ] ) {
     #ifndef _OPENMP
         fprintf( stderr, "OpenMP is not available\n" );
         return 1;
     #endif
-    float ang = (  30.*(float)NowMonth + 15.  ) * ( M_PI / 180. );
-    float temp = AVG_TEMP - AMP_TEMP * cos( ang );
-    unsigned int seed = 0;
-    NowTemp = temp + Ranf( &seed;, -RANDOM_TEMP, RANDOM_TEMP );
     
-    float precip = AVG_PRECIP_PER_MONTH + AMP_PRECIP_PER_MONTH * sin( ang );
-    NowPrecip = precip + Ranf( &seed;,  -RANDOM_PRECIP, RANDOM_PRECIP );
-    if( NowPrecip < 0. )
-        NowPrecip = 0.;
     omp_set_num_threads( 4 );    // same as # of sections
     #pragma omp parallel sections
     {
@@ -179,7 +187,7 @@ int main( int argc, char *argv[ ] ) {
         
         #pragma omp section
         {
-            MyAgent( );    // your own
+            Predator( );    // your own
         }
     }       // implied barrier -- all functions must return in order
     // to allow any of them to get past here
